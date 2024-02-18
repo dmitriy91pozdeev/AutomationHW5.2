0eb6e6b8634bc1e085293bc6aed25410ec3cc27a
Name Surname <dmitriy91pozdeev@mail.ru>
2024-02-18 17:56:14 +0300

    Help

--- src/test/java/ru/netology/testmode/test/DataGenerator.java
+++ src/test/java/ru/netology/testmode/test/DataGenerator.java
@@ -6,64 +6,77 @@ import io.restassured.filter.log.LogDetail;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
 import lombok.Value;
-import lombok.val;
+
 
 import java.util.Locale;
 
 import static io.restassured.RestAssured.given;
 
-    public class DataGenerator {
-        private static final RequestSpecification requestSpec = new RequestSpecBuilder()
-                .setBaseUri("http://localhost")
-                .setPort(9999)
-                .setAccept(ContentType.JSON)
-                .setContentType(ContentType.JSON)
-                .log(LogDetail.ALL)
-                .build();
-        private static final Faker faker = new Faker(new Locale("en"));
+public class DataGenerator {
+    private static final RequestSpecification requestSpec = new RequestSpecBuilder()
+            .setBaseUri("http://localhost")
+            .setPort(9999)
+            .setAccept(ContentType.JSON)
+            .setContentType(ContentType.JSON)
+            .log(LogDetail.ALL)
+            .build();
+    private static final Faker faker = new Faker(new Locale("en"));
 
-        private DataGenerator() {
+    private DataGenerator() {
+    }
+
+    private static RegistrationDto sendRequest(RegistrationDto user) {
+        given()
+                .spec(requestSpec)
+                .body(user)
+                .when()
+                .post("/api/system/users")
+                .then()
+                .statusCode(200);
+        return user;
+        // TODO: отправить запрос на указанный в требованиях path, передав в body запроса объект user
+        //  и не забудьте передать подготовленную спецификацию requestSpec.
+        //  Пример реализации метода показан в условии к задаче.
+    }
+
+    public static String getRandomLogin() {
+        return faker.name().username();
+
+        // TODO: добавить логику для объявления переменной login и задания её значения, для генерации
+        //  случайного логина используйте faker
+
+    }
+
+    public static String getRandomPassword() {
+        return faker.internet().password();
+        // TODO: добавить логику для объявления переменной password и задания её значения, для генерации
+        //  случайного пароля используйте faker
+
+    }
+
+    public static class Registration {
+        private Registration() {
         }
 
-        private static void sendRequest(RegistrationDto user) {
-            // TODO: отправить запрос на указанный в требованиях path, передав в body запроса объект user
-            //  и не забудьте передать подготовленную спецификацию requestSpec.
-            //  Пример реализации метода показан в условии к задаче.
+        public static RegistrationDto getUser(String status) {
+            return new RegistrationDto(getRandomLogin(), getRandomPassword(), status);
+            // TODO: создать пользователя user используя методы getRandomLogin(), getRandomPassword() и параметр status
+
         }
 
-        public static String getRandomLogin() {
-            // TODO: добавить логику для объявления переменной login и задания её значения, для генерации
-            //  случайного логина используйте faker
-            return login;
+        public static RegistrationDto getRegisteredUser(String status) {
+
+            return sendRequest(getUser(status));
+            // TODO: объявить переменную registeredUser и присвоить ей значение возвращённое getUser(status).
+            // Послать запрос на регистрацию пользователя с помощью вызова sendRequest(registeredUser)
+
         }
+    }
 
-        public static String getRandomPassword() {
-            // TODO: добавить логику для объявления переменной password и задания её значения, для генерации
-            //  случайного пароля используйте faker
-            return password;
-        }
-
-        public static class Registration {
-            private Registration() {
-            }
-
-            public static RegistrationDto getUser(String status) {
-                // TODO: создать пользователя user используя методы getRandomLogin(), getRandomPassword() и параметр status
-                return user;
-            }
-
-            public static RegistrationDto getRegisteredUser(String status) {
-                // TODO: объявить переменную registeredUser и присвоить ей значение возвращённое getUser(status).
-                // Послать запрос на регистрацию пользователя с помощью вызова sendRequest(registeredUser)
-                return registeredUser;
-            }
-        }
-
-        @Value
-        public static class RegistrationDto {
-            String login;
-            String password;
-            String status;
-        }
+    @Value
+    public static class RegistrationDto {
+        String login;
+        String password;
+        String status;
     }
 }
